b90f1ff72617cda2de42fb8a05c460a906679eda
Arch Dev Comp <patrick.rakowicz@gmail.com>
2022-08-24 09:49:42 -0600

    Lab1.cpp Created and tested with C++ CP

---
+++ CS-191/Lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Name: " << "Patrick" << "\n";
+	cout << "Favorite Color: " << "Blue/Purple" << "\n";
+	cout << "Ski?: " << "Yes" << "\n";
+	cout << "Haircut from Celeb? Who?: " << "No" << " - " << "No One" << "\n";
+	cout << "PB or Nutella?: " << "Penut Butter" << "\n";
+}
