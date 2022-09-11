beaf2ccea1c1ff3aa37d8b2e1a11f6c0de77ec05
Patrick Rakowicz <lividsubset3@gmail.com>
2022-09-10 21:32:18 -0600

    Finished | Debugged

--- CS-191/HW3/Main.cpp
+++ CS-191/HW3/Main.cpp
@@ -4,35 +4,27 @@
 using namespace std;
 
 namespace snowInfo {
-	int inches;
-//	string mountainRange;
-	float snowDensity;
+	double inches;
+	string mountainRange;
+    double snowDensity;
 	int time;
-
-	int SWE = inches * snowDensity;
-
 }
 
 int main() {
-	
+
 	cout << "Enter the new snow amount in inches: ";
 	cin >> snowInfo::inches;
-	cout << "\n";
 
-	cout << "Enter the mointain range: ";
-	string mountainRange;
-	getline(cin, mountainRange);
-//	getline(cin, snowInfo::mountainRange);
-	cout << "\n";
+	cout << "Enter the mountain range: ";
+    cin.ignore();
+    getline(cin, snowInfo::mountainRange);
 
 	cout << "Enter the snow density ( between 0 and 1): ";
 	cin >> snowInfo::snowDensity;
-	cout << "\n";
 
 	cout << "Enter the time (hours) that snow fell over: ";
 	cin >> snowInfo::time;
-	cout << "\n";
 
-	cout << snowInfo::inches << " Inches of snow fell in " << mountainRange << " over a period of " 
-		<< snowInfo::time << " hours, resulting in " << snowInfo::SWE << " inches of SWE.";
+	cout << snowInfo::inches << " Inches of snow fell in " << snowInfo::mountainRange << " over a period of "
+		<< snowInfo::time << " hours, resulting in " << snowInfo::inches * snowInfo::snowDensity << " inches of SWE.";
 }
