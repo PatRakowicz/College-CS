de9983a43aebdcda7a320ae5ec0de0ac0ac96f77
Arch Dev Comp <patrick.rakowicz@gmail.com>
2022-09-12 09:23:22 -0600

    Started | Not working

---
+++ CS-191/Lab4/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "Printing out string Vowels and Total Count." << "\n"; 
+	string mystr[i] = "Hello world, my name is sam!";
+	int total = 0;
+
+	if (mystr[i] == "a") {total =+ 1;} 
+	else if (mystr[i] == "e") {total =+ 1;} 
+	else if (mystr[i] == "i") {total =+ 1;} 
+	else if (mystr[i] == "o") {total =+ 1;} 
+	else if (mystr[i] == "u") {total =+ 1;}
+
+	cout << "Total #: " << total;
+
+}
