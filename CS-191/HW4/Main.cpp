125232c34856e301b820aea594edffbd73317473
Pat Mini <patrick.rakowicz@gmail.com>
2022-09-14 09:48:52 -0600

    started HW4

--- CS-191/HW4/Main.cpp
+++ CS-191/HW4/Main.cpp
@@ -2,4 +2,17 @@
 // Created by Pat Rak on 9/14/22.
 //
 
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+    string input = "y15ou a2iare333 do5i17ng gr42622aaea226t at262 t1ahis w232521jhole pro1kgram1lmi4542uhn1hg th98071iing";
+    for (int i = 0; i < input.length(); i++) {
+
+        if (input[i] == '1') {
+
+        }
+    }
+}
