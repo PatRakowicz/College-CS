e572802a6be58bbf855c5d28ee3c06e790279eec
Pat Mini <patrick.rakowicz@gmail.com>
2022-09-16 09:16:11 -0600

    Not finished | need debugged

--- CS-191/HW4/Main.cpp
+++ CS-191/HW4/Main.cpp
@@ -9,10 +9,12 @@ using namespace std;
 
 int main() {
     string input = "y15ou a2iare333 do5i17ng gr42622aaea226t at262 t1ahis w232521jhole pro1kgram1lmi4542uhn1hg th98071iing";
+    string output;
     for (int i = 0; i < input.length(); i++) {
-
         if (input[i] == '1') {
 
+        } else if (isdigit(input [i])) {
+            
         }
     }
 }
