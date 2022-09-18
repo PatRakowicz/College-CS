4ea53eecb68e25c4348cf7d39633a65a87080b61
Rako-BMac <patrick.rakowicz@gmail.com>
2022-09-17 22:05:05 -0600

    Done, working, Not complete output

--- CS-191/HW4/Main.cpp
+++ CS-191/HW4/Main.cpp
@@ -9,14 +9,16 @@ using namespace std;
 
 int main() {
     string input = "y15ou a2iare333 do5i17ng gr42622aaea226t at262 t1ahis w232521jhole pro1kgram1lmi4542uhn1hg th98071iing";
-    string output;
+
     for (int i = 0; i < input.length(); i++) {
-        if (input[i] != '1') {
-            input.append(output);
-        } else {
+        if (input[i] == '1') {
             i++;
+        } else if (input[i] == '2') {
+            i++;
+        } else if (isdigit(i) == true){
+            continue;
+        } else {
+            cout << input[i];
         }
     }
-
-    cout << output << endl;
 }
