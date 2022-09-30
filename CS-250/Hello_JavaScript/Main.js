d9b01f8fd61ef7beea3e707fd1afd0c8cdaa1381
Patrick Rakowicz <lividsubset3@gmail.com>
2022-09-30 10:20:26 -0600

    Finished | Debugged

--- CS-250/Hello_JavaScript/Main.js
+++ CS-250/Hello_JavaScript/Main.js
@@ -1,84 +1,21 @@
-/*
-var x = prompt('Enter Num: ');
+var x;
+var count = 0;
 
-let bool;
+do {
+    x = prompt('enter a non-zero positive int');
+} while (x <= 0);
 
-while (x < 0 && x !== 0) {
-    x = prompt("Empter Pos Number: ");
-    if (x < 0 && x !== 0) {
-        document.write("<ul>");
-        for (var i = 0; i < x; i++) {
-            console.log(i);
-            document.write("<li>" + i + "</li>");
-        }
-        document.write("</ul>");
-    } else {
-        console.log('error with wile loop');
+document.write('<h1> Factors of ', x, ':</h1>');
+
+for (var i = 1; i <= x; i++) {
+    if (x % i == 0) {
+        document.write('<p>', i, '</p>');
+        count++;
     }
 }
-*/
 
-var x = prompt('Enter a num');
-
-// if (x < 0) {
-//     x   = prompt('Enter Pos Num');
-// } else {
-//     document.write("<ul>");
-//     for (var i = 0; i < x; i++) {
-//         console.log(i);
-//         document.write("<li>" + i + "</li>");
-//     }
-//     document.write("<ul>");
-
-while (x < 0 && x != 0) {
-    x = prompt('Enter Pos Number');
+if (count <= 2) {
+    document.write('<h2>', x, ' is a prime</h2>')
+} else {
+    document.write('<h2>', x, ' is NOT a prime</h2>')
 }
-
-for (let i = 0; i <= x; i++) {
-    let flag = 0;
-
-    // looping through 2 to user input number
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) {
-            flag = 1;
-            break;
-        }
-    }
-
-    // if number greater than 1 and not divisible by other numbers
-    document.write("<ul>");
-    if (i > 1 && flag == 0) {
-        console.log(i);
-        document.write("<li>" + i + "</li>");
-    }
-    document.write("<ul>");
-}
-
-
-/*
-// program to print prime numbers between the two numbers
-
-// take input from the user
-const lowerNumber = parseInt(prompt('Enter lower number: '));
-const higherNumber = parseInt(prompt('Enter higher number: '));
-
-console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are:`);
-
-// looping from lowerNumber to higherNumber
-for (let i = lowerNumber; i <= higherNumber; i++) {
-    let flag = 0;
-
-    // looping through 2 to user input number
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) {
-            flag = 1;
-            break;
-        }
-    }
-
-    // if number greater than 1 and not divisible by other numbers
-    if (i > 1 && flag == 0) {
-        console.log(i);
-    }
-}
-*/
