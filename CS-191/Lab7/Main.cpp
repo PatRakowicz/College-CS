f50e43bfa8a65fba7c39fbfd772384169c6cb747
Arch Dev Comp <patrick.rakowicz@gmail.com>
2022-10-05 09:48:33 -0600

    Live Coding | working on Lab

--- CS-191/Lab7/Main.cpp
+++ CS-191/Lab7/Main.cpp
@@ -6,55 +6,39 @@
 Given an array of integers (you can hard code the array, you do not need to get user input),
  your goal is to determine if any 2 distinct integers in the array sum to a target integer t.
 
- 1. You must write a function called sums that accepts an integer array, an integer length of the array,
- and an integer that returns true if 2 distinct numbers in the array sum to tand false otherwise.
+ 1. You must write a function called sums that accepts an integer array, an
+    integer length of the array, and an integer t that returns true if 2 distinct
+    numbers in the array sum to t and false otherwise
 
  2. You must comment on the functionality, parameters, and return types of your functions
- above each function in a multi-line comment.
+    above each function in a multi-line comment.
 
  3. You must test this function on at least 3 different arrays/targets and show me the output.
 */
 
 #include <iostream>
-#include <random>
 
 using namespace std;
 
-int sum(int *arr, int T) {
+/*
+ * input array, check if two possible integers are sum to t int
+ *
+ * @param arr the main array that is inputted
+ * @param size the size of the main array
+ * @param t the input of sum that needs to add to it
+ * @return the sum of the number and if true or false
+ */
+int sum(int *arr, int size, int t) {
     int sum = 0;
-
-    // grab 2 rand elemnt from array
-    int random = rand() % 6;
-    int random2 = rand() % 6;
-
-    int getArrLen = sizeof(arr)/ sizeof(int);
-
-    int first = arr[random];
-    int second = arr[random2];
-
-    cout << "First Second";
-    cout << first << " " << second << endl;
-
-    // check if two ints are = to T (mainT)
-    int last = first + second;
-    if (last == T) {
-        cout << "if = " << last << endl;
-    } else {
-        cout << "False" << endl;
+    for(int i = 0; i < size; i++) {
+        if()
     }
-    // if 2 = to T, print
-    // else 2 != to T, repeat
-
-    for (int i = 0; i < getArrLen; i++) {
-        sum += arr[i];
-    }
-    return sum;
 }
 
 int main() {
-    int mainArr[]  = {11, 22, 5, 25, 15, 3,};
-    int size = 6;
-    int mainT = 44;
+    int mainArr[size]  = {1, 2, 3};
+    int size = 3;
+    int t = 44;
 
     cout << "func Sum : " << sum(mainArr, size) << endl;
 
@@ -63,5 +47,5 @@ int main() {
         cout << i << endl;
     }
 
-    cout << "Main T: " << mainT << endl;
+    cout << "Main T: " << t << endl;
 }
