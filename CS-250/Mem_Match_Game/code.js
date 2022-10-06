5b517fafd82e72d44292088c68d9be7985ac9a99
Rako-BMac <patrick.rakowicz@gmail.com>
2022-10-06 02:13:44 -0600

    Mem_Match | Table created with javascript | TODOs

--- CS-250/Mem_Match_Game/code.js
+++ CS-250/Mem_Match_Game/code.js
@@ -1,5 +1,25 @@
-
-
-function createTable() {
-
+function table(row, col) {
+    document.write('<table>');
+    for (let r = 0; r < row; r++) {
+        document.write('<tr>');
+        for( let c = 0; c < col; c++) {
+            document.write('<td>');
+            // TODO | input Function
+            //  Change 'text fill' with fnc randomizer
+            document.write('text fill');
+            document.write('</td>');
+        }
+        document.write('</tr>');
+    }
 }
+
+function randomizer() {
+    // TODO | Randomizer
+    //  loop random location for text
+    //  different everytime
+}
+
+let row = 3;
+let col = 4;
+
+table(row, col);
