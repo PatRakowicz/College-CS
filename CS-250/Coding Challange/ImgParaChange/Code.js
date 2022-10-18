6dfdad151bd6fc050004a2118fbacaa341920e4c
Arch Dev Comp <patrick.rakowicz@gmail.com>
2022-10-18 11:56:58 -0600

    Code Chalanage, only with color

--- CS-250/Coding Challange/ImgParaChange/Code.js
+++ CS-250/Coding Challange/ImgParaChange/Code.js
@@ -1,13 +1,10 @@
-let images = ['a.png', 'b.png', 'c.png']
-let currentImage = 0;
-
 let colors = ['black', 'blue', 'green', 'orange']
 let currentColor = 0;
 
 
-function image() {
-    currentImage += 1;
-    currentImage %= images.length;
-
-    img.src = images[currentImage];
+function color(p) {
+    currentColor += 1;
+    currentColor %= colors.length;
+    var p = document.getElementById('paragraph');
+    p.style = "color:  " + colors[currentColor];
 }
