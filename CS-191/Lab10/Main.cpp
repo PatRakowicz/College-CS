8cfc3ed32118c5d3d2602b59de76a0e181242bb6
Arch Dev Comp <patrick.rakowicz@gmail.com>
2022-10-26 09:47:02 -0600

    Lab10 | Finished | Debugged

--- CS-191/Lab10/Main.cpp
+++ CS-191/Lab10/Main.cpp
@@ -1,5 +1,46 @@
 //
 // Created by mo on 10/26/22.
 //
+#include <iostream>
 
-#include "Main.h"
+using namespace std;
+
+/**
+ * Swap two values in the array
+ *
+ * @param arr | Input given array to want swap ints
+ * @param i | Input first value int
+ * @param j | Input second value
+ */
+void arrSwapInt(int *arr, int i, int j) {
+    j = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i + 1] = j;
+}
+
+/**
+ * Bubble sort, take one int and check the array for a smaller value
+ * if value is smaller move to the front
+ *
+ * @param arr | Input the array to be sorted
+ * @param size | Input the size of given array
+ */
+void bubble_sort(int *arr, int size) {
+    for (int pos = 0; pos < size; pos++) {
+        for (int i = 0; i < size - pos; i++) {
+            if (arr[i] < arr[i + 1]) {
+                arrSwapInt(arr, i, i + 1);
+            }
+        }
+    }
+}
+
+
+int main() {
+    int x[5] = {1, 4, 6, 5, 8};
+    bubble_sort(x, 5);
+    for (int i: x) {
+        cout << i << endl;
+    }
+
+}
