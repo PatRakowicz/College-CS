dabc8f11cf59611cb1ea69efb361d74a3d73b767
Patrick Rakowicz <lividsubset3@gmail.com>
2022-11-04 21:02:03 -0600

    Small changes | less bugs

--- CS-191/LiveCoding/Encapsulation.cpp
+++ CS-191/LiveCoding/Encapsulation.cpp
@@ -4,40 +4,44 @@
 using namespace std;
 
 class classroom {
-	private:
-	public:
-		int capacity;
-		bool computers; // true if computers available, false otherwiase
-		bool chalkboards; // true if has chalkboards, false otherwise
-		int sq_feet;
-		classroom(); // default contrustor
-		classroom(int capacit, bool computers, bool chalkboards, int sq_feet);
-		void print_info();
+private:
+public:
+    int capacity;
+    bool computers; // true if computers available, false otherwiase
+    bool chalkboards; // true if has chalkboards, false otherwise
+    int sq_feet;
+
+    classroom(); // default contrustor
+    classroom(int capacity, bool computers, bool chalkboards, int sq_feet);
+
+    void print_info();
 }
 
 classroom::classroom() {
-	this->capacity = 0;
-	this->computers = false;
-	this->chalkboards = false;
-	this->sq_feet = 0;
+    this->capacity = 0;
+    this->computers = false;
+    this->chalkboards = false;
+    this->sq_feet = 0;
 }
 
-classroom::classroom(int capacity, bool computers, boo chalkboards, int sq_feet) {
-	this->capacity = capacity;
-	this->computers = computers;
-	this->chakboards = chalkboards;
-	this->sq_feet = sq_feet;
+classroom::classroom(int capacity, bool computers, bool chalkboards, int sq_feet) {
+    this->capacity = capacity;
+    this->computers = computers;
+    this->chalkboards = chalkboards;
+    this->sq_feet = sq_feet;
 }
 
 void classroom::print_info() {
-	cout << capacity << endl;
-	cout << computers << endl;
-	cout << chalkboars << endl;
-	cout << sq_feet << endl;
+    cout << capacity << endl;
+    cout << computers << endl;
+    cout << chalkboards << endl;
+    cout << sq_feet << endl;
+
+}
 
 int main() {
-	classroom c1(25, false, false, 1000);
-	c1.print_info();
+    classroom c1(25, false, false, 1000);
+    c1.print_info();
 
-	return 0;
+    return 0;
 }
