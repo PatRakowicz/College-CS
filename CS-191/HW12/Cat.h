1848d64240374015f5a8bf917970c14c659f058d
Arch Dev Comp <patrick.rakowicz@gmail.com>
2022-11-11 09:37:55 -0700

    HW12 | done | Debugged

--- CS-191/HW12/Cat.h
+++ CS-191/HW12/Cat.h
@@ -5,48 +5,131 @@
 #ifndef LAB12_MAIN_H
 #define LAB12_MAIN_H
 
-
+template<class T>
 class Cat {
 private:
     string name;
-    int age;
-    int weight;
+    T age;
+    T weight;
 public:
     Cat();
+    Cat(string u_name, T u_age, T u_weight);
     void set_name(string u_name);
-    void set_age(int u_age);
-    void set_weight(int u_weight);
+    void set_age(T u_age);
+    void set_weight(T u_weight);
     void print();
+    string get_name();
+    T get_age();
+    T get_weight();
 };
 
-Cat::Cat() {
+/** Default Constructor, set all values to 0 or ""
+ *
+ * @tparam T | Class template for int and float
+ */
+template<class T>
+Cat<T>::Cat() {
     name = "";
     age = 0;
     weight = 0;
 }
 
-void Cat::set_name(string u_name) {
+/** Input constructor for one line
+ *
+ * @tparam T | Class template for values
+ * @param u_name | input value to have user input name
+ * @param u_age | input value to be set in main function
+ * @param u_weight | input value t get set in main function with float or int
+ */
+template<class T>
+Cat<T>::Cat(string u_name, T u_age, T u_weight) {
+    name = u_name;
+
+    if (u_age < 0)
+        cout << "Error, need valid age" << endl;
+    else
+        age = u_age;
+
+    if (u_weight < 0)
+        cout << "Error, need valid weight" << endl;
+    else
+        weight = u_weight;;
+}
+
+/** Set function to name
+ *
+ * @tparam T | class template to follow
+ * @param u_name | input string name from user
+ */
+template<class T>
+void Cat<T>::set_name(string u_name) {
     name = u_name;
 }
 
-void Cat::set_age(int u_age) {
-    if(u_age < 0)
-        cout << "Can not go below 0" << endl;
+/** Set function for age
+ *
+ * @tparam T | class template to follow
+ * @param u_age | input age from user, check if input is valid
+ */
+template<class T>
+void Cat<T>::set_age(T u_age) {
+    if (u_age < 0)
+        cout << "Error, need valid age" << endl;
     else
         age = u_age;
 }
 
-void Cat::set_weight(int u_weight) {
-    if(u_weight < 0)
-        cout << "Can not set below 0" << endl;
+/** Setter from user to input weight
+ *
+ * @tparam T | class template to follow
+ * @param u_weight | Input from user, check if input is valid
+ */
+template<class T>
+void Cat<T>::set_weight(T u_weight) {
+    if (u_weight < 0)
+        cout << "Error, need valid weight" << endl;
     else
         weight = u_weight;
 }
 
-void Cat::print() {
-    cout << "Name : " << name << endl;
-    cout << "Age: " << age << endl;
-    cout << "Weight: " << weight << endl;
+/** Print constructor
+ *
+ * @tparam T | class template to follow
+ */
+template<class T>
+void Cat<T>::print() {
+    cout << name << " is " << age << " year(s) old and weighs "
+         << weight << "lbs" << endl;
+}
+
+/** Get Name constructor
+ *
+ * @tparam T | class template to follow
+ * @return | name of the given cat
+ */
+template<class T>
+string Cat<T>::get_name() {
+    return name;
+}
+
+/** Get Age constructor
+ *
+ * @tparam T | class template to follow
+ * @return | age of the given cat
+ */
+template<class T>
+T Cat<T>::get_age() {
+    return age;
+}
+
+/** Get weight of given cat
+ *
+ * @tparam T | class template to follow
+ * @return | Weight of given cat, w/ param in main
+ */
+template<class T>
+T Cat<T>::get_weight() {
+    return weight;
 }
 
 
--- CS-191/HW12/Main.cpp
+++ CS-191/HW12/Main.cpp
@@ -3,15 +3,23 @@
 //
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 #include "Cat.h"
 
 int main() {
-    Cat c1;
-    c1.set_name("Bobby");
-    c1.set_weight(12);
-    c1.set_age(8);
-    c1.print();
+    Cat<int> kitty;
+    kitty.set_name("Ms. Kittens");
+    kitty.set_age(-1);
+    kitty.set_weight(-12);
+    kitty.set_age(10);
+    kitty.set_weight(13);
+    kitty.print();
+
+    Cat<float> c("Jerry", 10.16, 15.3);
+    c.print();
+    c.set_weight(18.5);
+    c.print();
     return 0;
 }
