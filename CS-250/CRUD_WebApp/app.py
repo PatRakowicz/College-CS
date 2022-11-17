2c8c35968ec77129186555ae5fbf0e345bd757b0
Arch Dev Comp <patrick.rakowicz@gmail.com>
2022-11-16 18:43:01 -0700

    CRUD_WA | Debugged | Finished

---
+++ CS-250/CRUD_WebApp/app.py
@@ -0,0 +1,67 @@
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Fruit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    color = db.Column(db.String(100))
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        fruit = request.form["fruit"]
+        color = request.form["color"]
+
+        _entry = Fruit(name=fruit, color=color)
+
+        db.session.add(_entry)
+        db.session.commit()
+
+        return redirect('read')
+    else:
+        return render_template('create.html')
+
+
+@app.route('/read')
+def read():
+    list = Fruit.query.all()
+    return render_template('read.html', list=list)
+
+
+@app.route('/update/<id>', methods=['POST', 'GET'])
+def update(id):
+    target = Fruit.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        target.name = request.form['fruit']
+        target.color = request.form['color']
+
+        db.session.commit()
+
+        return redirect('/read')
+    else:
+        return render_template('update.html', fruit=target.name, color=target.color, id=id)
+
+
+@app.route('/delete/<id>')
+def delete(id):
+    target = Fruit.query.filter_by(id=id).first()
+    db.session.delete(target)
+    db.session.commit()
+    return redirect('/read')
+
+
+app.app_context().push()
+
+if __name__ == "__main__":
+    app.run()
