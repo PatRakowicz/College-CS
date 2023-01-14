e11dc2484fcccdfaf37c1f2543f91c9ef3a30ff4
Patrick Rakowicz <lividsubset3@gmail.com>
2023-01-13 18:56:07 -0700

    Working read file

--- CS-280/HW1/Main.cpp
+++ CS-280/HW1/Main.cpp
@@ -1,39 +1,56 @@
+//
+// Created by Livid on 13/01/2023.
+//
+
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <algorithm>
+#include <tuple>
+#include <utility>
 
 using namespace std;
 
-// Download .txt and read assignment
-// download example code for class
-//  take file input
-// create main .cpp with file input
-//  Run .cpp and read/print each line from .txt
-
-// make item struct that holds a type, status, price
-// splitting each line from file around commas and putting these into a struct
-//  string-stream for slitting
-// create array of size 100 that holds struct, put each struct into array
-// handle additional logic for adding/removing from array
-
-
-struct garageSale {
+struct marketFormat {
     string type;
+    string stat;
     bool status;
-    double price;
+    int price;
 };
 
-int main() {
-    string input_line;
-    ifstream f("data.txt");
 
-    if (f.is_open()) {
-        cout << "Reading from file \n";
-        while (getline(f, input_line)) {
-            cout << input_line << endl;
-        }
-        f.close();
-    } else {
+int main() {
+    string winFile = "m:/livid/desktop/dev/college-cs/cs-280/hw1/data.txt";
+    string MacFile = "/{dir}/data.txt";
+
+    marketFormat items[17];
+    ifstream file(winFile);
+    string line;
+    int index = 0;
+
+    if (!file.is_open()) {
         cout << "File does not exists \n";
+        return 1;
     }
+
+    while (getline(file, line)) {
+        line.erase(remove(line.begin(), line.end(), ' '), line.end());
+        replace(line.begin(), line.end(), ',', ' ');
+
+        stringstream ss(line);
+
+        ss >> items[index].type >> items[index].stat >> items[index].price;
+
+        if (items[index].stat == "forsale") { items[index].status = true; }
+        else { items[index].status = false; }
+        index++;
+    }
+    file.close();
+
+    for (int i = 0; i < index; i++) {
+        cout << items[i].type << " " << boolalpha << items[i].status << " " << items[i].price << endl;
+    }
+
+    return 0;
 }
