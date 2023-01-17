1c3f13e2f7ba1edb1e8eca6ca27e40ffc066ac7c
Rako-BMac <patrick.rakowicz@gmail.com>
2023-01-17 14:44:03 -0700

    Project Started

---
+++ CS-350/HW2-colorDB/SQLscript.cpp
@@ -0,0 +1,13 @@
+// Make a script that parses the file given to individual cadegorys and input them into a created database that has
+// Color name (Name of color)
+// HEX Value (######)
+// RGB (255,255,255)
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  // Start to parse and create database inport script
+}
