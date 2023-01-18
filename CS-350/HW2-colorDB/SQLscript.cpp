07b3042b98a47ecb088b00226898849ca1f36cf4
Patrick Rakowicz <lividsubset3@gmail.com>
2023-01-17 18:26:56 -0700

    Parse file and uploaded to SQL

--- CS-350/HW2-colorDB/SQLscript.cpp
+++ CS-350/HW2-colorDB/SQLscript.cpp
@@ -1,13 +1,34 @@
-// Make a script that parses the file given to individual cadegorys and input them into a created database that has
+// Make a script that parses the file given to individual cadegorys
+// and input them into a created database that has
 // Color name (Name of color)
 // HEX Value (######)
 // RGB (255,255,255)
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
+string NAME;
+string HEX;
+int rgb[3];
+
 int main() {
-  // Start to parse and create database inport script
+    ifstream file("M:/Livid/Desktop/Dev/College-CS/CS-350/HW2-colorDB/colors.txt");
+    string line;
+    ofstream sqlFile("M:/Livid/Desktop/Dev/College-CS/CS-350/HW2-colorDB/Inserted_Colors.sql");
+    sqlFile << "CREATE TABLE colors (name VARCHAR(255), hex VARCHAR(7), red INT, green INT, blue INT);" << endl;
+
+    while (getline(file, line)) {
+        replace(line.begin(), line.end(), ',', ' ');
+
+        stringstream ss(line);
+        ss >> NAME >> HEX >> rgb[0] >> rgb[1] >> rgb[2];
+
+        sqlFile << "INSERT INTO colors (name, hex, red, green, blue) VALUES ('" <<
+                NAME << "', '" << HEX << "', " << rgb[0] << ", " << rgb[1] << ", " << rgb[2] << ");" << endl;
+    }
 }
