140bbf21241438ba3d762c88d13cf00ea44a964d
Patrick Rakowicz <lividsubset3@gmail.com>
2023-01-23 21:52:19 -0700

    Finished Logic | Small problem Microwave

--- CS-280/HW1/Main.cpp
+++ CS-280/HW1/Main.cpp
@@ -53,91 +53,75 @@ vector<marketFormat> readFile(const string &fileName) {
 
 /* Check data given and remove what is equal to some others
  *
- * @param string | File input that is being read by readFile function
+ * @param vector<market> | input given array of data
  * */
-void checkData(string inputFile) {
-    vector<marketFormat> dataArray = readFile(inputFile);
-
+void checkData(const vector<marketFormat>& dataArray) {
     if (dataArray.empty()) {
         cout << "No Data / Empty Array \n";
     }
 
+    vector<marketFormat> forSale;
+    vector<marketFormat> wanted;
+    vector<marketFormat> matchedItems;
+
     for (auto &i: dataArray) {
-        cout << i.type << " " << boolalpha << i.status << " " << i.price << endl;
-    }
-
-    cout << endl << "~~~~ Checking Data ~~~~" << endl << endl;
-
-    for (const auto& data : dataArray) {
-        if(data.type == "bike") {
-            cout << "Type: " << data.type;
-            if(data.status) {
-                cout << " | For Sale | Price:" << data.price << endl;
-            } else {
-                cout << " | Wanted | Price:"<< data.price << endl;
-            }
+        if (i.status) {
+            forSale.push_back(i);
+        } else {
+            wanted.push_back(i);
         }
     }
 
-    cout << endl << "~~~~ Checking Data ~~~~" << endl << endl;
-
-    for (const auto& data : dataArray) {
-        if(data.type == "microwave") {
-            cout << "Type: " << data.type;
-            if(data.status) {
-                cout << " | For Sale | Price:" << data.price << endl;
-            } else {
-                cout << " | Wanted | Price:"<< data.price << endl;
+    for (int i = 0; i < wanted.size(); i++) {
+        int j = 0;
+        while (j < forSale.size()) {
+            if (wanted[i].type == forSale[j].type) {
+                if (wanted[i].price >= forSale[j].price) {
+                    matchedItems.push_back(forSale[j]);
+                    matchedItems.push_back(wanted[i]);
+                    forSale.erase(forSale.begin() + j);
+                    wanted.erase(wanted.begin() + i);
+                    i--;
+                    break;
+                }
             }
+            j++;
         }
     }
 
-    cout << endl << "~~~~ Checking Data ~~~~" << endl << endl;
+    cout << endl << "For Sale" << endl;;
 
-    for (const auto& data : dataArray) {
-        if(data.type == "dresser") {
-            cout << "Type: " << data.type;
-            if(data.status) {
-                cout << " | For Sale | Price:" << data.price << endl;
-            } else {
-                cout << " | Wanted | Price:"<< data.price << endl;
-            }
-        }
+    for (auto &i: forSale) {
+        cout << "Name: " << i.type << ", Price: " << i.price << ", Status: ";
+        cout << (i.status ? "For Sale" : "Wanted") << endl;
     }
 
-    cout << endl << "~~~~ Checking Data ~~~~" << endl << endl;
+    cout << endl << "Wanted" << endl;
 
-    for (const auto& data : dataArray) {
-        if(data.type == "truck") {
-            cout << "Type: " << data.type;
-            if(data.status) {
-                cout << " | For Sale | Price:" << data.price << endl;
-            } else {
-                cout << " | Wanted | Price:"<< data.price << endl;
-            }
-        }
+    for (auto &i: wanted) {
+        cout << "Name: " << i.type << ", Price: " << i.price << ", Status: ";
+        cout << (i.status ? "For Sale" : "Wanted") << endl;
     }
 
-    cout << endl << "~~~~ Checking Data ~~~~" << endl << endl;
+    cout << endl << "Sold" << endl;
 
-    for (const auto& data : dataArray) {
-        if(data.type == "chicken") {
-            cout << "Type: " << data.type;
-            if(data.status) {
-                cout << " | For Sale | Price:" << data.price << endl;
-            } else {
-                cout << " | Wanted | Price:"<< data.price << endl;
-            }
+    for (auto &i: matchedItems) {
+        if (i.status) {
+            cout << "Type: " << i.type << " Price: " << i.price << endl;
         }
+//        cout << "Name: " << i.type << ", Price: " << i.price << ", Status: ";
+//        cout << (i.status ? "For Sale" : "Wanted") << endl;
     }
-
 }
 
 int main() {
+    // change the file for the location
+    // it does not work for me if I just include the file "data.txt"
     string winFile = "m:/livid/desktop/dev/college-cs/cs-280/hw1/data.txt";
     string macFile = "/Users/patrak/Documents/Dev/College-CS/CS-280/HW1/data.txt";
 
-    checkData(macFile);
+    string mainTest = "data.txt";
+    checkData(readFile(winFile));
 
     return 0;
 }
