45ebf30aea4f18fad159f1421547ae5d221be562
Rako-BMac <patrick.rakowicz@gmail.com>
2023-01-26 18:24:58 -0700

    Finished | Working

--- CS-350/HW3-CRWebApp/hw03.sql
+++ CS-350/HW3-CRWebApp/hw03.sql
@@ -2,12 +2,12 @@ CREATE DATABASE cs_350;
 
 USE cs_350;
 
-CREATE TABLE products (
-    ID INT PRIMARY KEY,
-    brand VARCHAR(255),
-    name VARCHAR(255),
+CREATE TABLE bikes (
+    ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    brand VARCHAR(30),
+    name VARCHAR(30),
     price DECIMAL(10,2),
-    year INT
+    year INT(6)
 );
 
 GRANT ALL PRIVILEGES ON cs_350.*
