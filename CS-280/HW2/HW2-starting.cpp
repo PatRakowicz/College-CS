d7093b84a603f4feb6c9ca7a36ca4d5fc2536a71
Rako-BMac <patrick.rakowicz@gmail.com>
2023-01-27 12:31:35 -0700

    Project Init | working on

--- CS-280/HW2/HW2-starting.cpp
+++ CS-280/HW2/HW2-starting.cpp
@@ -9,6 +9,7 @@
 #include <fstream> // for file IO
 #include <typeinfo> // for typeid
 #include <sstream> // for splitting strings
+
 using namespace std;
 
 // array doubles variable
@@ -17,10 +18,9 @@ int numDoubles = 0;
 int numDuplicates = 0;
 
 // must use this struct
-struct wordItem
-{
-	string word;
-	int count;
+struct wordItem {
+    string word;
+    int count;
 };
 
 /**
@@ -39,7 +39,9 @@ struct wordItem
 *		reassign wordItemList to the new array
 * Note: can call with doubleArraySize(wordItemList, length);
 */
-void doubleArraySize(wordItem *&wordItemList, int &length);
+void doubleArraySize(wordItem *&wordItemList, int &length) {
+
+}
 
 /*
 * Function name: addWord
@@ -55,7 +57,9 @@ void doubleArraySize(wordItem *&wordItemList, int &length);
 * 		case we might have to double the size if wordIndex == length
 * 		and add a new wordItem with count 1
 */
-void addWord(wordItem *&wordItemList, int &wordIndex, int &length, string nextWord);
+void addWord(wordItem *&wordItemList, int &wordIndex, int &length, string nextWord) {
+
+}
 
 
 /*
@@ -67,7 +71,9 @@ void addWord(wordItem *&wordItemList, int &wordIndex, int &length, string nextWo
 * should be called with arraySort(wordItemList, wordIndex)
 * can modify the array directly (list[0] = something)
 */
-void arraySort(wordItem wordItemList[], int wordIndex);
+void arraySort(wordItem wordItemList[], int wordIndex) {
+
+}
 
 
 /*
@@ -78,7 +84,9 @@ wordItems
 * @param topN - the number of top frequency words to print
 * @return none
 */
-void printTopN(wordItem wordItemList[], int topN);
+void printTopN(wordItem wordItemList[], int topN) {
+
+}
 
 
 
@@ -86,12 +94,10 @@ void printTopN(wordItem wordItemList[], int topN);
 /**** feel free to add any other functions you need here ****/
 /************************************************************/
 
-void printArr(wordItem wordItemList[], int wordIndex)
-{
-	for(int i = 0; i < wordIndex; i++)
-	{
-		cout << wordItemList[i].word << ", " << wordItemList[i].count << endl;
-	}
+void printArr(wordItem wordItemList[], int wordIndex) {
+    for (int i = 0; i < wordIndex; i++) {
+        cout << wordItemList[i].word << ", " << wordItemList[i].count << endl;
+    }
 }
 
 
@@ -100,28 +106,36 @@ void printArr(wordItem wordItemList[], int wordIndex)
 * take care of reading in the file and creating the arrays here
 */
 
-int main()
-{
-	int wordIndex = 0; // keep track of where to insert new wordItems
-	int length = 1; // keep track of total size of array
-	wordItem* wordItemList = new wordItem[length]; // create a word item array starting with 1 spot
+int main() {
+    int wordIndex = 0; // keep track of where to insert new wordItems
+    int length = 1; // keep track of total size of array
+    wordItem *wordItemList = new wordItem[length]; // create a word item array starting with 1 spot
 
-	// get command line arguments
-	int topN = 4;
-	string inputFile = "test.txt";
+    // get command line arguments
+    int topN = 4;
+    string inputFile = "shakespeare.txt";
 
 
-	// (1) parse line by line from file
-	//   (1a) break up lines by spaces to get each word
-	//   (1b) put each word into the array
-	//        if there is already a wordItem for the word, increase the count
-	// 		  if the word is not in the array, create a new wordItem with count = 1
-	// (2) sort your array of wordItems
-	// (3) print out the top n and their frequency
-	// (4) print out number of array doubles (global variable up top for this)
-	// (5) print out total number of unique words
-	// (6) print out total number of duplicate words
+    ifstream file(inputFile);
+    string line;
 
-	delete [] wordItemList; // clean up
-	return 0; //return "success" status
+    if (!file.is_open()) { cout << "File Does not Exists \n"; }
+
+    while (getline(file, line)) {
+
+    }
+
+    // (1) parse line by line from file
+    //   (1a) break up lines by spaces to get each word
+    //   (1b) put each word into the array
+    //        if there is already a wordItem for the word, increase the count
+    // 		  if the word is not in the array, create a new wordItem with count = 1
+    // (2) sort your array of wordItems
+    // (3) print out the top n and their frequency
+    // (4) print out number of array doubles (global variable up top for this)
+    // (5) print out total number of unique words
+    // (6) print out total number of duplicate words
+
+    delete[] wordItemList; // clean up
+    return 0; //return "success" status
 }
