9746679ad251d83b8d953e8142f3ba32877112e5
Rako-BMac <patrick.rakowicz@gmail.com>
2023-02-21 14:54:33 -0700

    HW7 | Testing

---
+++ CS-350/HW7-Hangman/hw07.php
@@ -0,0 +1,20 @@
+<?php
+ $selWords = array();
+
+ function testLoadHTML() {
+    echo "
+    <h1> Hangman </h1>
+    <h2>This is a test to check if it will load on call of function testLoadHTML</h2>
+    ";
+ };
+
+
+ testLoadHTML();
+?>
+
+<head>
+
+</head>
+<body>
+
+</body>
