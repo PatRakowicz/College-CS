f538a3efd21bce5b5a7853df302a9de03a7e5456
Patrick Rakowicz <lividsubset3@gmail.com>
2023-02-24 13:42:59 -0700

    HW7 | Working | Debugged | Using Sessions

--- CS-350/HW7-Hangman/hw07.php
+++ CS-350/HW7-Hangman/hw07.php
@@ -1,52 +1,75 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['word'])) {
-    $words = array("apple", "banana", "cherry", "date", "elderberry", "fig");
-    $_SESSION['word'] = str_split(strtoupper($words[rand(0, count($words) - 1)]));
-    $_SESSION['guess'] = array_fill(0, count($_SESSION['word']), "*");
-    $_SESSION['attempts'] = 0;
-    $_SESSION['missed'] = array();
-}
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $words = array("apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon");
+    $selected_word = $words[array_rand($words)];
+    $_SESSION["selected_word"] = $selected_word;
+    $_SESSION["user_guess"] = str_repeat("*", strlen($selected_word));
+    $_SESSION["attempts"] = 0;
+    $_SESSION["missed_letters"] = "";
+    print($selected_word);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['guess'])) {
-    $guess = strtoupper($_POST['guess']);
-    if (in_array($guess, $_SESSION['word'])) {
-        for ($i = 0; $i < count($_SESSION['word']); $i++) {
-            if ($_SESSION['word'][$i] == $guess) {
-                $_SESSION['guess'][$i] = $guess;
+} elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $selected_word = $_SESSION["selected_word"];
+    $user_guess = $_SESSION["user_guess"];
+    $attempts = $_SESSION["attempts"];
+    $missed_letters = $_SESSION["missed_letters"];
+
+    $letter = strtolower($_POST["letter"]);
+
+    if (strlen($letter) != 1) {
+        echo "Please enter only one character.";
+        exit();
+    }
+
+    if (strpos($selected_word, $letter) !== false) {
+        for ($i = 0; $i < strlen($selected_word); $i++) {
+            if ($selected_word[$i] == $letter) {
+                $user_guess[$i] = $letter;
             }
         }
-        if (!in_array("*", $_SESSION['guess'])) {
-            echo "Congratulations, you guessed the word: " . implode("", $_SESSION['guess']) . "!";
-            session_destroy();
-            exit;
-        }
+        $_SESSION["user_guess"] = $user_guess;
     } else {
-        $_SESSION['attempts']++;
-        $_SESSION['missed'][] = $guess;
-        if ($_SESSION['attempts'] == 3) {
-            echo "Game over, the word was: " . implode("", $_SESSION['word']) . ".";
-            session_destroy();
-            exit;
-        }
+        $attempts++;
+        $missed_letters .= $letter;
+        $_SESSION["attempts"] = $attempts;
+        $_SESSION["missed_letters"] = $missed_letters;
+    }
+
+    if ($user_guess == $selected_word) {
+        echo "<h1>HangMan Game</h1>";
+        echo "Congratulations, you guessed the word!";
+        exit();
+    }
+
+    if ($attempts >= 3) {
+        echo "<h1>HangMan Game</h1>";
+        echo "Game over. The word was " . $selected_word . ".";
+        exit();
     }
 }
-
 ?>
 
+
 <head>
-    <title>Hangman</title>
+    <title>Hangman Game</title>
 </head>
+
 <body>
-<h1>Hangman</h1>
-<p>Guess the word:</p>
-<p><?php echo implode(" ", $_SESSION['guess']); ?></p>
-<p>Attempts: <?php echo $_SESSION['attempts']; ?></p>
-<p>Missed: <?php echo implode(", ", $_SESSION['missed']); ?></p>
-<form method="post">
-    <input type="text" name="guess" maxlength="1" autocomplete="off">
-    <button type="submit">Guess</button>
-<!--    <button type="button" onclick="--><?php //session_destroy(); ?><!--">Reload</button>-->
-</form>
+<h1>HangMan Game</h1>
+
+<p>User's current guess: <?php echo $_SESSION["user_guess"]; ?></p>
+<p>Number of attempts: <?php echo $_SESSION["attempts"]; ?></p>
+<p>Letters missed so far: <?php echo $_SESSION["missed_letters"]; ?></p>
+
+<?php if ($_SESSION["attempts"] < 3 && $_SESSION["user_guess"] != $_SESSION["selected_word"]) { ?>
+    <form method="POST" action="">
+        <label for="letter">Enter a single letter: </label>
+        <input type="text" name="letter" id="letter" maxlength="1">
+        <input type="submit" value="Guess">
+    </form>
+<?php } ?>
+
 </body>
+
