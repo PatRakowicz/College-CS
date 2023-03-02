aca325f86dd391a8aac1d7ba01aec16638999275
Patrick Rakowicz <lividsubset3@gmail.com>
2023-03-02 14:09:44 -0700

    HW8 | ++ Session timeout | Working | Finished

--- CS-350/HW8-ColorCart/cart.php
+++ CS-350/HW8-ColorCart/cart.php
@@ -1,18 +1,21 @@
 <?php
+session_set_cookie_params(10, '/');
+
 session_start();
+
 if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = array();
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Add selected color to cart
-    $selected_color = $_POST['color'];
-    if (!in_array($selected_color, $_SESSION['cart'])) {
-        $_SESSION['cart'][] = $selected_color;
+    if (isset($_POST['color'])) {
+        $selected_color = $_POST['color'];
+        if (!in_array($selected_color, $_SESSION['cart'])) {
+            $_SESSION['cart'][] = $selected_color;
+        }
     }
 }
 
-// Remove selected colors from cart
 if (isset($_POST['remove'])) {
     foreach ($_POST['remove'] as $color) {
         $key = array_search($color, $_SESSION['cart']);
@@ -22,7 +25,15 @@ if (isset($_POST['remove'])) {
     }
 }
 
-// Display cart
+function removeColor() {
+    $unique_colors = array_unique($_SESSION['cart']);
+    foreach ($unique_colors as $color) {
+        echo "<input type='checkbox' name='remove[]' value='$color'>";
+        echo "<span style='display:inline-block;width:70px;height:70px;background-color:$color;'></span>";
+        echo "<br>";
+    }
+}
+
 ?>
 
 <head>
@@ -31,16 +42,8 @@ if (isset($_POST['remove'])) {
 <body>
 <h1>Cart</h1>
 <p>Selected colors:</p>
-<form action="cart.php" method="POST">
-    <?php
-    // Display each color in the cart
-    $unique_colors = array_unique($_SESSION['cart']);
-    foreach ($unique_colors as $color) {
-        echo "<input type='checkbox' name='remove[]' value='$color'>";
-        echo "<span style='display:inline-block;width:70px;height:70px;background-color:$color;'></span>";
-        echo "<br>";
-    }
-    ?>
+<form method="POST">
+    <?php removeColor(); ?>
     <button type="submit">Remove selected colors</button>
 </form>
 <a href="index.html">Add Color</a>
