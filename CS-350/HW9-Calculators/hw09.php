a9a3332cf863c2c51a54a1a26388d999c9975667
Rako-BMac <patrick.rakowicz@gmail.com>
2023-03-07 20:14:15 -0700

    HW9 | Working | Debugged

--- CS-350/HW9-Calculators/hw09.php
+++ CS-350/HW9-Calculators/hw09.php
@@ -1,134 +1,162 @@
 <?php
-session_start();
-
-function arithmetic($x, $y, $operation)
+function calculate($x, $y, $operations)
 {
-    switch ($operation) {
-        case 'addition':
-            return $x + $y;
-        case 'subtraction':
-            return $x - $y;
-        case 'multiplication':
-            return $x * $y;
-        case 'division':
-            return $x / $y;
-        case 'exponentiation':
-            return pow($x, $y);
-        case 'modulo':
-            return $x % $y;
-        default:
-            return null;
+    $results = array();
+
+    foreach ($operations as $operation) {
+        switch ($operation) {
+            case 'addition':
+                $results[] = $x + $y;
+                break;
+            case 'subtraction':
+                $results[] = $x - $y;
+                break;
+            case 'multiplication':
+                $results[] = $x * $y;
+                break;
+            case 'division':
+                if ($y == 0) {
+                    $results[] = "Error: division by zero";
+                } else {
+                    $results[] = $x / $y;
+                }
+                break;
+            case 'exponentiation':
+                $results[] = pow($x, $y);
+                break;
+            case 'modulo':
+                if ($y == 0) {
+                    $results[] = "Error: division by zero";
+                } else {
+                    $results[] = $x % $y;
+                }
+                break;
+        }
     }
+
+    return $results;
 }
 
-function factorial($fac)
+function factorial($n)
 {
-    if ($fac <= 1) {
+    if ($n < 0) {
+        return "Invalid input";
+    } else if ($n == 0) {
         return 1;
     } else {
-        return $fac * factorial($fac - 1);
+        $result = 1;
+        for ($i = 1; $i <= $n; $i++) {
+            $result *= $i;
+        }
+        return $result;
     }
 }
 
-function fibonacci($fib)
+function fibonacci($n)
 {
-    if ($fib <= 0) {
+    if ($n < 0) {
+        return "Invalid input";
+    } else if ($n == 0) {
         return 0;
-    } else if ($fib == 1) {
+    } else if ($n == 1 || $n == 2) {
         return 1;
     } else {
-        return fibonacci($fib - 1) + fibonacci($fib - 2);
+        $fibonacci = array(0, 1, 1);
+        for ($i = 3; $i <= $n; $i++) {
+            $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+        }
+        return $fibonacci[$n];
     }
 }
 
-// Arithmetic Post Method
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['operation']) && $_POST['operation'] == 'arithmetic') {
-    $x = $_POST['x'];
-    $y = $_POST['y'];
-    $operation = $_POST['operation'];
-
-    $result = arithmetic($x, $y, $operation);
-    $_SESSION['arithmetic_result'] = $result;
-}
-
-// Factorial Post Method
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['operation']) && $_POST['operation'] == 'factorial') {
-    $fac = $_POST['fac'];
-
-    $result = factorial($fac);
-    $_SESSION['factorial_result'] = $result;
-}
-
-// Fibonacci Post Method
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['operation']) && $_POST['operation'] == 'fibonacci') {
-    $fib = $_POST['fib'];
-
-    $result = fibonacci($fib);
-    $_SESSION['fibonacci_result'] = $result;
-}
 ?>
 
 <head>
-    <title>Calculator's</title>
+    <title>Calculator</title>
 </head>
 <body>
-
 <h1>Arithmetic Calculator</h1>
-<form method="POST" action="">
-    <input type="hidden" name="operation" value="arithmetic">
-    <label for="x">Value of X:</label>
-    <input type="number" step="any" name="x" required><br><br>
-    <label for="y">Value of Y:</label>
-    <input type="number" step="any" name="y" required><br><br>
-    <label for="operation">Operation:</label>
-    <select name="operation">
-        <option value="addition">Addition (+)</option>
-        <option value="subtraction">Subtraction (-)</option>
-        <option value="multiplication">Multiplication (*)</option>
-        <option value="division">Division (/)</option>
-        <option value="exponentiation">Exponentiation (^)</option>
-        <option value="modulo">Modulo (%)</option>
-    </select><br><br>
-    <input type="submit" value="Calculate">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="number" name="x" placeholder="Enter First Val">
+    <input type="number" name="y" placeholder="Enter Second Val">
+    <br>
+    <input type="checkbox" name="operations[]" value="addition"> Addition
+    <input type="checkbox" name="operations[]" value="subtraction"> Subtraction
+    <input type="checkbox" name="operations[]" value="multiplication"> Multiplication
+    <input type="checkbox" name="operations[]" value="division"> Division
+    <input type="checkbox" name="operations[]" value="exponentiation"> Exponentiation
+    <input type="checkbox" name="operations[]" value="modulo"> Modulo
+    <br>
+    <input type="submit" name="submit1" value="Calculate">
 </form>
+
 <?php
-// Arith
-if (isset($_SESSION['arithmetic_result'])) {
-    echo "<p>The result is: " . $_SESSION['arithmetic_result'] . "</p>";
-    unset($_SESSION['arithmetic_result']);
+if (isset($_POST['submit1'])) {
+    $x = isset($_POST['x']) ? $_POST['x'] : null;
+    $y = isset($_POST['y']) ? $_POST['y'] : null;
+    $operations = isset($_POST['operations']) ? $_POST['operations'] : array();
+    $results = array();
+
+    if (empty($x) || empty($y)) {
+        $results[] = "Please enter two numbers";
+    } else {
+        $results = calculate($x, $y, $operations);
+    }
+
+    echo "<h2>Result:</h2>";
+    foreach ($results as $result) {
+        echo "<p>";
+        if (is_numeric($result)) {
+            echo "$x + $y = $result";
+        } else {
+            echo $result;
+        }
+        echo "</p>";
+    }
 }
 ?>
 
 <h1>Factorial Calculator</h1>
-<form method="POST" action="">
-    <input type="hidden" name="operation" value="factorial">
-    <label for="fac">Value of N:</label>
-    <input type="number" name="fac" min="0" required><br><br>
-    <input type="submit" value="Calculate">
-
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="number" name="n" placeholder="Enter a number">
+    <input type="submit" name="submit2" value="Calculate">
 </form>
+
 <?php
-// Fac
-if (isset($_SESSION['factorial_result'])) {
-    echo "<p>The factorial of " . $_SESSION['factorial_value'] . " is: " . $_SESSION['factorial_result'] . "</p>";
-    unset($_SESSION['factorial_result']);
-    unset($_SESSION['factorial_value']);
+if (isset($_POST['submit2'])) {
+    $n = isset($_POST['n']) ? $_POST['n'] : null;
+    $result = null;
+
+    if (empty($n)) {
+        $result = "Please enter a number";
+    } else {
+        $result = factorial($n);
+    }
+
+    echo "<h2>Result:</h2>";
+    echo "<p>$n! = $result</p>";
 }
 ?>
 
 <h1>Fibonacci Calculator</h1>
-<form method="POST" action="">
-    <input type="hidden" name="operation" value="fibonacci">
-    <label for="fib">Value of N:</label>
-    <input type="number" name="fib" min="1" required><br><br>
-    <input type="submit" value="Calculate">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="text" name="n" placeholder="Enter a number">
+    <input type="submit" name="submit3" value="Calculate">
 </form>
+
 <?php
-// Fib
-if (isset($_SESSION['fibonacci_result'])) {
-    echo "<p>The " . $_SESSION['fibonacci_value'] . "th Fibonacci number is: " . $_SESSION['fibonacci_result'] . "</p>";
-    unset($_SESSION['fibonacci_result']);
-    unset($_SESSION['fibonacci_value']);
+if (isset($_POST['submit3'])) {
+    $n = isset($_POST['n']) ? $_POST['n'] : null;
+    $result = null;
+
+    if (empty($n)) {
+        $result = "Please enter a number";
+    } else {
+        $result = fibonacci($n);
+    }
+
+    echo "<h2>Result:</h2>";
+    echo "<p>The $n-th Fibonacci number is $result</p>";
 }
 ?>
 </body>
