b558b61ea6d3285fbb24420673fa72a9e51b399d
Patrick Rakowicz <lividsubset3@gmail.com>
2023-09-14 13:09:45 -0600

    CS235 | Sockets

--- CS-235/Sockets/socket_assignment_clientside.py
+++ CS-235/Sockets/socket_assignment_clientside.py
@@ -7,39 +7,47 @@ from socket import *
 
 """
 TODO:
-Change socket to use TCP
+    Change socket to use TCP
 """
 #Create socket
-clientsocket = socket(AF_INET, SOCK_DGRAM)
+clientsocket = socket(AF_INET, SOCK_STREAM)
 
 hostaddress='localhost'
 portnumber = 3333
 
-sendingaddress = (hostaddress, portnumber)
+clientsocket.connect((hostaddress, portnumber))
 
 """
 TODO:
-1. Edit to use TCP
-HINT: you will need to establish a connection and change the sendto function
-2. Edit "message" to send a GET request for /index.html page
+    1. Edit to use TCP
+    HINT: you will need to establish a connection and change the sendto function
+    2. Edit "message" to send a GET request for /index.html page
 """
-message = "I am sending data..."
+message = "GET /help.html HTTP/1.1\r\nHost: {}\r\n\r\n".format(hostaddress)
 
 """
 QUESTION:
-Give an example of a different HTTP request.
-Answer: 
+    Give an example of a different HTTP request.
+Answer: An example of a different HTTP request is a POST request 
+        used to submit data to be processed to a specified resource.
 """
 
-clientsocket.sendto(message.encode(), sendingaddress)
+clientsocket.send(message.encode())
 
 """
 TODO:
-Receive ALL data, decode message and print message to console
-HINT: you will need a while loop to receive all data;
-check whether the data being received is not empty and break when it is.
+    Receive ALL data, decode message and print message to console
+    HINT: you will need a while loop to receive all data;
+    check whether the data being received is not empty and break when it is.
 """
-receivemessage = clientsocket.recv(1024).decode()
+full_data = []
+while True:
+    data = clientsocket.recv(1024)
+    if not data:
+        break
+    full_data.append(data.decode())
+received_message = ''.join(full_data)
+print(received_message)
 
 #close connection
 clientsocket.close()
--- CS-235/Sockets/socket_assignment_serverside.py
+++ CS-235/Sockets/socket_assignment_serverside.py
@@ -10,18 +10,23 @@ serversocket = socket(AF_INET, SOCK_STREAM)
 
 """
 QUESTION:
-Why do we define a portnumber?
-Answer: 
+        Why do we define a portnumber?
+Answer: We define a port number to identify a specific process to which data is to be sent once the 
+        IP address has been reached. It allows multiple processes to listen and send data on the same 
+        IP address without interfering with each other.
 """
-#Prepare a server socket
-hostaddress='localhost'
+# Prepare a server socket
+hostaddress = 'localhost'
 portnumber = 3333
 serversocket.bind((hostaddress, portnumber))
 
 """
 QUESTION:
-What does the parameter 1 in the listen function indicate?
-Answer: 
+    What does the parameter 1 in the listen function indicate?
+Answer: The parameter '1' in the listen function indicates the maximum number of queued connections. 
+        In this case, it means that if one connection is already being handled, the server will keep 
+        one more connection request waiting in a queue. If a third connection request comes in while these 
+        two are being handled, it would be refused.
 """
 serversocket.listen(1)
 
@@ -34,18 +39,31 @@ while True:
     2. Add a try/catch to catch page not found error and return the 404error.html page
     HINT: check message being received for what page is being requested
     """
-    print('server is running on port ', portnumber)
-    #Establish the connection
+    print('Server is running on port: ', portnumber)
+    # Establish the connection
     connectionsocket, clientaddress = serversocket.accept()
-    
-    #receive the message
+
+    # receive the message
     message = connectionsocket.recv(1024).decode()
 
-    #This sends the HTTP header and Hello World response into socket
-    connectionsocket.send("HTTP/1.0 200 OK\r\nHello World\r\n".encode())
+    print("Received from client:", message)
 
+    try:
+        filename = message.split(' ')[1]
+        print(f"Loading page: {filename[1:]}")
+        with open(filename[1:], 'r') as f:
+            outputdata = f.read()
+            connectionsocket.send("HTTP/1.0 200 OK\r\n\r\n".encode())
+            for line in outputdata:
+                connectionsocket.send(line.encode())
+
+    except FileNotFoundError:
+        print(f"Page {filename[1:]} not found, sending 404 error.")
+        with open("404error.html", 'r') as f:
+            error_page = f.read()
+            connectionsocket.send("HTTP/1.0 404 Not Found\r\n\r\n".encode())
+            connectionsocket.send(error_page.encode())
+
+    # This sends the HTTP header and Hello World response into socket
     connectionsocket.close()
-
-         
 serversocket.close()
-
