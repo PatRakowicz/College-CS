7258a173121b593504e0ddf9b4ff86ba17c2adf9
Patrick Rakowicz <lividsubset3@gmail.com>
2023-11-16 19:53:27 -0700

    CS330 | HW13 | SJF implementation, needs testing

--- CS-330/HW13-CPUScheduler/SJF_Scheduler.cpp
+++ CS-330/HW13-CPUScheduler/SJF_Scheduler.cpp
@@ -3,5 +3,50 @@
 SJF_Scheduler::SJF_Scheduler(int sim_time) : Scheduler(sim_time){}
 
 void SJF_Scheduler::run(){
-  //CODE GOES HERE
+	list<Process *> ready;
+	Process *currentProcess = nullptr;
+	int minCycles;
+
+	while(t < sim_time){
+		// Check for arriving processes
+		for(auto &proc_pair : processes){
+			Process *proc = proc_pair.second;
+			if(proc->arrival_t <= t && proc->status == INACTIVE){
+				proc->status = READY;
+				proc->wait_t = t - proc->arrival_t;
+				ready.push_back(proc);
+				cout << "Time " << t << ": Process " << proc->pid << " is ready" << endl;
+			}
+		}
+
+		// Select process with the shortest job
+		if (!ready.empty()) {
+			auto shortestJob = ready.begin();
+			minCycles = (*shortestJob)->cycles;
+			for (auto it = ready.begin(); it != ready.end(); ++it) {
+				if ((*it)->cycles < minCycles) {
+					shortestJob = it;
+					minCycles = (*it)->cycles;
+				}
+			}
+			currentProcess = *shortestJob;
+			ready.erase(shortestJob);
+
+			// Execute the shortest job
+			currentProcess->status = RUNNING;
+			cout << "Time " << t << ": Process " << currentProcess->pid << " starts running" << endl;
+			t += currentProcess->cycles; // Simulate process running to completion
+			currentProcess->cycles = 0;
+			currentProcess->status = TERMINATED;
+			done.push_back(currentProcess); // Add to done list
+			cout << "Time " << t << ": Process " << currentProcess->pid << " terminates" << endl;
+
+			// Update wait times for other processes
+			for (auto &proc : ready) {
+				proc->wait_t += currentProcess->cycles;
+			}
+		} else {
+			t++;  // Increment time if no process is ready
+		}
+	}
 }
