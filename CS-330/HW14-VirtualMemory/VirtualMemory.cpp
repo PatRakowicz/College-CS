de3762c8c995bdbefb10bf3de20755b307759db3
Rako-BMac <patrick.rakowicz@gmail.com>
2023-12-02 17:47:10 -0700

    CS330 | HW14 | Updated Page fault & Found

--- CS-330/HW14-VirtualMemory/VirtualMemory.cpp
+++ CS-330/HW14-VirtualMemory/VirtualMemory.cpp
@@ -1,7 +1,7 @@
 #include "hw14.h"
 
 // Executed on MAC
-byte& VirtualMemory::operator[](const int addr) {
+byte &VirtualMemory::operator[](const int addr) {
     refs++;
 
     if (addr < 0 || addr >= 65536) {
@@ -19,23 +19,25 @@ byte& VirtualMemory::operator[](const int addr) {
     for (int i = 0; i < TABLE_SIZE; i++) {
         if (page_table[i] == pageNumber) {
             frameNumber = i;
-
-            cerr << "PAGE FOUND!" << endl;
-            cerr << "   page table entry: " << i << endl;
-            cerr << "   physical address: " << (frameNumber * 32 + pageOffset) << endl;
             break;
         }
     }
 
     if (frameNumber == -1) {
         faults++;
-
         cerr << "PAGE FAULT!" << endl;
 
         int replaceFrame = next;
         next = (next + 1) % TABLE_SIZE;
 
-        cerr << "   empty slot in page table: " << replaceFrame << endl;
+        if (page_table[replaceFrame] == -1) {
+            cerr << "    empty slot in page table: " << replaceFrame << endl;
+        } else {
+            cerr << "    NO SPACE in page table" << endl;
+            cerr << "    page OUT: " << page_table[replaceFrame] << endl;
+        }
+
+        cerr << "    page IN:  " << pageNumber << endl;
 
         if (page_table[replaceFrame] != -1) {
             disk.write_page(page_table[replaceFrame], &main_memory[replaceFrame * 32]);
@@ -44,8 +46,9 @@ byte& VirtualMemory::operator[](const int addr) {
         disk.read_page(pageNumber, &main_memory[replaceFrame * 32]);
         page_table[replaceFrame] = pageNumber;
         frameNumber = replaceFrame;
-
-        cerr << "reading disk memory page: " << pageNumber << endl;
+    } else {
+        cerr << "PAGE FOUND!" << endl;
+        cerr << "   page table entry: " << frameNumber << endl;
     }
 
     int physicalAddr = frameNumber * 32 + pageOffset;
@@ -55,30 +58,26 @@ byte& VirtualMemory::operator[](const int addr) {
 }
 
 //DO NOT MODIFY BELOW 
-VirtualMemory::VirtualMemory()
-{
-  for (int i = 0; i < TABLE_SIZE; i++) {
-    page_table[i] = -1;
-  }
+VirtualMemory::VirtualMemory() {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        page_table[i] = -1;
+    }
 
-  next = 0;
-  faults = 0;
-  refs = 0;
+    next = 0;
+    faults = 0;
+    refs = 0;
 }
 
-float VirtualMemory::getFaultRate()
-{
-  return 100 * (faults / (double) refs); 
+float VirtualMemory::getFaultRate() {
+    return 100 * (faults / (double) refs);
 }
 
-void VirtualMemory::clearFaultRate()
-{
-  faults = refs = 0;
+void VirtualMemory::clearFaultRate() {
+    faults = refs = 0;
 }
 
-void VirtualMemory::print_table()
-{
-  for (int i = 0 ; i < TABLE_SIZE; i++) {
-    cout << "page_table[" << i << "] = " << page_table[i] << endl;
-  }
+void VirtualMemory::print_table() {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        cout << "page_table[" << i << "] = " << page_table[i] << endl;
+    }
 }
