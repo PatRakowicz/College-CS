431406d67a173855eb43093ec98b4376df0ffb25
Rako-BMac <patrick.rakowicz@gmail.com>
2024-01-12 17:05:04 -0700

    CS 412 | HW1

--- CS-412/HW1-NumberGuess/src/Main.java
+++ CS-412/HW1-NumberGuess/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 0;
+        int number;
         number = (int) (Math.random() * 100);
         System.out.println(number);
     }
