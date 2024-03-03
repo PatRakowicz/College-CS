26cc754834d7384ad87faff1e49209cbc7c7b3e4
Pat Rako <patrick.rakowicz@gmail.com>
2024-03-02 23:26:30 -0700

    CS 412 | removed comments

--- CS-412/HW8-FixGUI/src/StudentView.java
+++ CS-412/HW8-FixGUI/src/StudentView.java
@@ -69,7 +69,6 @@ public class StudentView {
             }
         });
 
-        // Action listener for the update button
         jButton.addActionListener(e -> {
             String selectedValue = jList.getSelectedValue();
             if (selectedValue != null && !selectedValue.isEmpty()) {
