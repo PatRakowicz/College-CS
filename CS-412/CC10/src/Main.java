2f6c1833799b23fff08ee3a06fb5acac89680080
Pat Rako <patrick.rakowicz@gmail.com>
2024-03-20 12:16:01 -0600

    CC10 | CS 412 | Changed to pring 10 threads

--- CS-412/CC10/src/Main.java
+++ CS-412/CC10/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println("Thread: " + i + " | Howdy");
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Howdy");
                 }
-            }
-        });
-        thread.start();
+            });
+            thread.start();
+        }
     }
 }
