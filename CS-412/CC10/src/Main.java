82a09cb13d7046743761b5558b622e7cf5500556
Pat Rako <patrick.rakowicz@gmail.com>
2024-03-20 12:23:27 -0600

    CC10 | CS 412 | Print Main at end

--- CS-412/CC10/src/Main.java
+++ CS-412/CC10/src/Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Thread> t =new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             Thread thread = new Thread(new Runnable() {
                 @Override
@@ -7,7 +10,17 @@ public class Main {
                     System.out.println("Howdy");
                 }
             });
+            t.add(thread);
             thread.start();
         }
+
+        for (Thread th : t) {
+            try {
+                th.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Main");
     }
 }
