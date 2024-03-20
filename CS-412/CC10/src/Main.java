e684f024dce17520da54fc3853fe8754fcf402f0
Pat Rako <patrick.rakowicz@gmail.com>
2024-03-20 12:14:34 -0600

    CC10 | CS 412 | moved from .go()

--- CS-412/CC10/src/Main.java
+++ CS-412/CC10/src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        new Main().go();
-    }
-
-    public void go() {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                for(int i = 0; i< 10; i ++) {
+                for (int i = 0; i < 10; i++) {
                     System.out.println("Thread: " + i + " | Howdy");
                 }
             }
