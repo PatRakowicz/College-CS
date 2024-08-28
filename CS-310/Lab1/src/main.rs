6f504de1a83487b7a6dff35d55dc196cafe152c5
Pat Rako <patrick.rakowicz@gmail.com>
2024-08-28 13:51:00 -0600

    CS 310 | Lab1

---
+++ CS-310/Lab1/src/main.rs
@@ -0,0 +1,24 @@
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("Guess the Number");
+    let number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Line was not read");
+
+        let guess = guess.trim().parse().expect("Err not number");
+
+        if number < guess {
+            println!("Too High");
+        } else if number > guess {
+            println!("Too Low");
+        } else {
+            println!("Correct");
+            break;
+        }
+    }
+}
