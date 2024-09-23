093242f6e3d599f069d5236446a86fe841f5f176
Dev-Linux <patrick.rakowicz@gmail.com>
2024-09-23 12:56:07 -0600

    Act5 Init

---
+++ CS-435/Act5/app/src/main/java/Beer.java
@@ -0,0 +1,3 @@
+public class Beer(var name :String?=null, var description :String?=null, var imageResourceID :Int?=null) {
+
+}
---
+++ CS-435/Act5/app/src/main/java/cs435/act5/BeerCategoryActivity.kt
@@ -0,0 +1,25 @@
+package cs435.act5
+
+import Beer
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BeerCategoryActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_beer_category)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val beer = Beer();
+    }
+}
