33d1f6be9d3556ad71b4e0599139af728af76adb
Dev-Linux <patrick.rakowicz@gmail.com>
2024-10-02 12:48:28 -0600

    CS435 | Exam1 Finished

--- CS-435/Exam1/app/src/main/java/cs435/exam1/MainActivity.kt
+++ CS-435/Exam1/app/src/main/java/cs435/exam1/MainActivity.kt
@@ -1,12 +1,23 @@
 package cs435.exam1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val numberList = ArrayList<Int>()
+    private lateinit var listView : ListView
+    private lateinit var buttonNew : Button
+    private lateinit var buttonSum : Button
+    private lateinit var buttonClear : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +29,52 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        listView = findViewById(R.id.listView)
+        buttonNew = findViewById(R.id.buttonNew)
+        buttonSum = findViewById(R.id.buttonSum)
+        buttonClear = findViewById(R.id.buttonClear)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, numberList)
+        listView.adapter = adapter
+
+        buttonNew.setOnClickListener {
+            val newNumber = Random.nextInt(0, 10)
+            numberList.add(newNumber)
+            adapter.notifyDataSetChanged()
+        }
+
+        buttonSum.setOnClickListener {
+            val sum = calculateSum(numberList)
+            Toast.makeText(this, "Total Sum: $sum", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonClear.setOnClickListener {
+            numberList.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        listView.setOnItemClickListener { _, _, i, _ ->
+            numberList[i] = -numberList[i]
+            adapter.notifyDataSetChanged()
+        }
+
+        listView.setOnItemLongClickListener { _, _, i, _ ->
+            numberList.removeAt(i)
+            adapter.notifyDataSetChanged()
+            true
+        }
+    }
+
+    private fun calculateSum(numbers: List<Int>): Int {
+        var sum = 0
+        for (number in numbers) {
+            sum += number
+        }
+        return sum
     }
 }
+
+
+
+
+
