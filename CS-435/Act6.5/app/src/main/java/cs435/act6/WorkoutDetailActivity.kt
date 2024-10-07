5ba326f249bbe3d906b9a5418dfa8a0da5d0403a
Dev-Linux <patrick.rakowicz@gmail.com>
2024-10-07 12:47:34 -0600

    CS435 | Act6.5

---
+++ CS-435/Act6.5/app/src/main/java/cs435/act6/WorkoutDetailActivity.kt
@@ -0,0 +1,29 @@
+package cs435.act6
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WorkoutDetailActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_workout_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val intent = intent
+        val workoutID = intent.getIntExtra("ID", 0)
+        val workoutDetailFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as? WorkoutDetailFragment
+        workoutDetailFragment?.setWorkoutID(workoutID)
+
+    }
+}
