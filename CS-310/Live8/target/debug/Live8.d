04286c8492f7814f6a775a03e35939bf8f3ae5ed
BMac-Dev <patrick.rakowicz@gmail.com>
2024-10-14 15:00:58 -0600

    CS310 | Live8 WIP

---
+++ CS-310/Live8/target/debug/Live8.d
@@ -0,0 +1,1 @@
+/Users/rako/Documents/Dev/College-CS/CS-310/Live8/target/debug/Live8: /Users/rako/Documents/Dev/College-CS/CS-310/Live8/src/main.rs
---
+++ CS-310/Live8/target/debug/deps/Live8-5710054f8588ef59.d
@@ -0,0 +1,5 @@
+/Users/rako/Documents/Dev/College-CS/CS-310/Live8/target/debug/deps/Live8-5710054f8588ef59: src/main.rs
+
+/Users/rako/Documents/Dev/College-CS/CS-310/Live8/target/debug/deps/Live8-5710054f8588ef59.d: src/main.rs
+
+src/main.rs:
