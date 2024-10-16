7393a6880ef7aefd7e957d7911056836be940b6c
BMac-Dev <patrick.rakowicz@gmail.com>
2024-10-16 13:21:26 -0600

    CS310 | Live8 finished

--- CS-310/Live8/src/main.rs
+++ CS-310/Live8/src/main.rs
@@ -1,9 +1,10 @@
-use std::{env, io};
+use std::{env, fs, io};
+use std::error::Error;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read};
 use log::error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // env::set_var("RUST_BACKTRACE=1", "1");
     // panic!("text panic")
 
@@ -17,15 +18,15 @@ fn main() {
     //  Ok(result) and Err(error
     //
     let file_result_enum = File::open("afile.txt");
-    /*let file = match file_result_enum {
+    let file = match file_result_enum {
         Ok(file) => file,
         Err(error) => panic!("File did not want to open today")
-    };*/
+    };
 
-    // let file = File::open("afile.txt").unwrap();
-    /*let file = File::open("hello.txt").expect("file cannot be found");
+    let file = File::open("afile.txt").unwrap();
+    let file = File::open("hello.txt").expect("file cannot be found");
 
-    let file = match file_result_enum {
+    /*let file = match file_result_enum {
         Ok(file) => file,
         Err(error) => match error.kind() {
             io::ErrorKind::NotFound => match File::create("hello.txt") {
@@ -34,29 +35,27 @@ fn main() {
                     panic!("Cannot create the file")
                 },
             },
-            _ => {panic!("Could not open file")},
+            _ => { panic!("Could not open file") },
         }
-    };
+    };*/
 
     let file = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == io::ErrorKind::NotFound {
             File::create("hello.txt").unwrap_or_else(|error| panic!("No Open"))
         } else {
-        panic!("other problems");
+            panic!("other problems");
         }
     });
-}*/
+
+    Ok()
 }
 fn read_content_from_file() -> Result<String, io::Error> {
-    let fresult = File::open("afile.txt");
-    let mut file = match fresult {
-        Ok(file) => file,
-        Err(error) => return Err(error)
-    };
-    let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Ok(_) => Ok(contents),
-        Err(e) => Err(e)
-    }
+    // let mut fresult = File::open("afile.txt")?;
+    // let mut contents = String::new();
+    // File::open("afile.txt")?.read_to_string(&mut contents)?;
+    // fresult.read_to_string(&mut contents)?;
+    // Ok(contents);
+
+    fs::read_to_string("afile.txt")
 }
 
