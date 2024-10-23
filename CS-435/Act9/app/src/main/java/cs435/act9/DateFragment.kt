ff340160f35434e86099455d839d79a7fb28834e
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-10-23 12:58:04 -0600

    CS435 | Act9 datePicker

---
+++ CS-435/Act9/app/src/main/java/cs435/act9/DateFragment.kt
@@ -0,0 +1,41 @@
+package cs435.act9
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.icu.util.Calendar
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+
+class DateFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_date, container, false)
+
+        return view
+
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(requireActivity(), this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val message = String.format("%d/%d/%d", year, month + 1, dayOfMonth)
+        Toast.makeText(requireActivity().applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+}
--- CS-435/Act9/app/src/main/java/cs435/act9/MainActivity.kt
+++ CS-435/Act9/app/src/main/java/cs435/act9/MainActivity.kt
@@ -1,8 +1,14 @@
 package cs435.act9
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,6 +17,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,5 +28,46 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menuItemA -> {
+                Toast.makeText(this, "A was clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.menuItemB -> {
+                Toast.makeText(this, "B was clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun showAlertDialog(view: View) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("I am an alert")
+        builder.setMessage("Is ralph funny or sad?")
+
+        builder.setPositiveButton("Funny") { dialog, which ->
+            Toast.makeText(this,"funny", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("Sad") { dialog, which ->
+            Toast.makeText(this, "sad", Toast.LENGTH_SHORT).show()
+        }
+
+
+        builder.show()
+    }
+    // Lab9 Time picker
+    // Create time picker fragment
+    fun onClickDatePicker(view: View) {
+        val dateFragment = DateFragment()
+        dateFragment.show(supportFragmentManager, null)
     }
 }
