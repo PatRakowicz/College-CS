01aa722a80513ee5c969b2421c154c5c366462a4
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-10-25 16:13:11 -0600

    CS435 | Lab9 Finished

--- CS-435/Act9 - Copy/app/src/main/java/cs435/act9/MainActivity.kt
+++ CS-435/Act9 - Copy/app/src/main/java/cs435/act9/MainActivity.kt
@@ -1,36 +1,30 @@
 package cs435.act9
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
+import android.widget.TimePicker
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cs435.act9.TimeFragment
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), TimeFragment.OnTimeSetListener {
 
-    private lateinit var timeText : TextView
+    private lateinit var timeText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        timeText = findViewById(R.id.PickTime)
-
-
+        timeText = findViewById(R.id.timeTextView)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -41,11 +35,17 @@ class MainActivity : AppCompatActivity(){
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.PickTime -> {
-                val dateFragment = TimeFragment()
-                dateFragment.show(supportFragmentManager, null)
+                val timeFragment = TimeFragment()
+                timeFragment.setOnTimeSetListener(this)
+                timeFragment.show(supportFragmentManager, null)
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onTimeSelected(hourOfDay: Int, minute: Int) {
+        val time = String.format("the time is | %02d:%02d", hourOfDay, minute)
+        timeText.text = time
     }
 }
