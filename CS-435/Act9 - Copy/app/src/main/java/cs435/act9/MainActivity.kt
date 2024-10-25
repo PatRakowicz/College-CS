fd8aca633ad082c48a9e66a00c6152695301d187
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-10-25 12:55:58 -0600

    CS 435 | Lab9

---
+++ CS-435/Act9 - Copy/app/src/main/java/cs435/act9/MainActivity.kt
@@ -0,0 +1,51 @@
+package cs435.act9
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var timeText : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        timeText = findViewById(R.id.PickTime)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.PickTime -> {
+                val dateFragment = TimeFragment()
+                dateFragment.show(supportFragmentManager, null)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
