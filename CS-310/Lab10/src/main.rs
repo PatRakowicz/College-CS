0eb49576fd1f4a228584fc0297f578dace08e69d
BMac-Dev <patrick.rakowicz@gmail.com>
2024-11-01 13:16:43 -0600

    CS310 | Lab10 finished

--- CS-310/Lab10/src/main.rs
+++ CS-310/Lab10/src/main.rs
@@ -4,6 +4,7 @@ use std::io::Write;
 use std::sync::mpsc::Receiver;
 use std::sync::{mpsc, Arc, Mutex};
 use std::{fs, thread, vec};
+use std::ffi::c_long;
 use std::time::Duration;
 use rand::Rng; // Add `rand` crate to cargo.toml: rand = "0.8.4"
 
@@ -33,33 +34,37 @@ fn main() {
     let (log_tx, log_rx) = mpsc::channel();
 
     // TODO: Initialize a LogReceiver using the just initialized receiver object
-    let mut logRecever = LogReceiver { receiver };
+    let mut logRecever = LogReceiver { receiver: log_rx };
 
     // TODO: Spawn a SINGLE thread to handle logging, 
     // inside the thread call the logging method we defined for our LogReceiver struct 
     thread::spawn(move || { logRecever.logging() });
 
     // TODO: Use a for loop to spawn multiple threads to handle "requests" (minimum of 5 threads)
-    {
+    let mut handles = vec![];
+    for i in 0..5 {
         // TODO: Clone the transmitter
-
+        let tx_clone = log_tx.clone();
         // TODO: spawn threads
-        {
+        let handle = thread::spawn(move || {
             // random number generator to vary the duration of thread spawning
             let duration = rand::thread_rng().gen_range(1..4);
 
             // TODO: use generated duration to make thread sleep
+            thread::sleep(Duration::new(duration, 0));
 
             // Generate log message
             let log_message = format!("Request {} processed in {} seconds", i, duration);
 
             //TODO: Send log message over transmitter
-        }
+            tx_clone.send(log_message)
+        });
+        handles.push(handle)
     }
 
     //TODO: release transmitter by dropping it
+    drop(log_tx);
 
     //TODO: Call join() on appropriate handle to make sure threads do not end prematurely
-
-
+    for handle in handles { handle.join().unwrap(); }
 }
