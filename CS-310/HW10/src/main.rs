8854c059673a91ac4db0403a5b753b83b5c38b71
BMac-Dev <patrick.rakowicz@gmail.com>
2024-11-04 15:41:24 -0700

    CS310 | HW10 finished

--- CS-310/HW10/src/main.rs
+++ CS-310/HW10/src/main.rs
@@ -5,6 +5,7 @@ use std::sync::mpsc::Receiver;
 use std::sync::{mpsc, Arc, Mutex};
 use std::{fs, thread, vec};
 use std::ffi::c_long;
+use std::fmt::format;
 use std::time::Duration;
 use rand::Rng; // Add `rand` crate to cargo.toml: rand = "0.8.4"
 
@@ -12,12 +13,14 @@ use rand::Rng; // Add `rand` crate to cargo.toml: rand = "0.8.4"
 // TODO: add variable receiver of type mpsc::Receiver<> which will receive String messages
 struct LogReceiver {
     receiver: mpsc::Receiver<String>,
+    loggingFile: File
 }
 
 //implemented constructor and function "logging"
 impl LogReceiver {
-    fn new(rx: Receiver<String>) -> Self {
-        Self { receiver: rx}
+    fn new(rx: Receiver<String>, file: &str) -> Self {
+        let loggingFile = File::create(file).expect("~~File created Failed~~");
+        Self { receiver: rx, loggingFile}
     }
     // Logging function: prints out the log message received
     // We are checking if the receiver return on Ok(String) value by assigning the value to a variable
@@ -25,6 +28,9 @@ impl LogReceiver {
     fn logging(&mut self) {
         while let Ok(log) = self.receiver.recv() {
             println!("Log: {}", log);
+            self.loggingFile
+                .write_all(format!("Log: {}\n", log).as_bytes())
+                .expect("~~File write FAILED~~");
         }
     }
 }
@@ -34,17 +40,21 @@ fn main() {
     let (log_tx, log_rx) = mpsc::channel();
 
     // TODO: Initialize a LogReceiver using the just initialized receiver object
-    let mut logRecever = LogReceiver { receiver: log_rx };
+    let mut logRecever = LogReceiver::new(log_rx, "log.txt");
 
     // TODO: Spawn a SINGLE thread to handle logging, 
     // inside the thread call the logging method we defined for our LogReceiver struct 
     thread::spawn(move || { logRecever.logging() });
+
+    let count = Arc::new(Mutex::new(0));
 
     // TODO: Use a for loop to spawn multiple threads to handle "requests" (minimum of 5 threads)
     let mut handles = vec![];
     for i in 0..5 {
         // TODO: Clone the transmitter
         let tx_clone = log_tx.clone();
+        let count_clone = Arc::clone(&count);
+
         // TODO: spawn threads
         let handle = thread::spawn(move || {
             // random number generator to vary the duration of thread spawning
@@ -57,7 +67,9 @@ fn main() {
             let log_message = format!("Request {} processed in {} seconds", i, duration);
 
             //TODO: Send log message over transmitter
-            tx_clone.send(log_message)
+            tx_clone.send(log_message).unwrap();
+
+            *count_clone.lock().unwrap() += i;
         });
         handles.push(handle)
     }
@@ -67,4 +79,6 @@ fn main() {
 
     //TODO: Call join() on appropriate handle to make sure threads do not end prematurely
     for handle in handles { handle.join().unwrap(); }
+
+    println!("Final Count: {}", *count.lock().unwrap())
 }
