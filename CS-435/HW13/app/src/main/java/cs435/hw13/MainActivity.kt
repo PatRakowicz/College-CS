72613b8ff18a2b4f59cf3888959cbe8303918e7c
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-11-23 14:05:48 -0700

    CS435 | HW13 init

---
+++ CS-435/HW13/app/src/main/java/cs435/hw13/MainActivity.kt
@@ -0,0 +1,52 @@
+package cs435.hw13
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.find_action -> {
+                // find logic
+                return true
+            }
+
+            R.id.favList_action -> {
+                // return favorite list activity
+                return true
+            }
+
+            R.id.favorite_action -> {
+                // add to favorite data model
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
