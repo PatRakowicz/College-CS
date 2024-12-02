7dc2bb63cfdf52b98b6cd28515d5bf2ec1b01c43
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-12-02 12:42:13 -0700

    CS 435 | Act14 WIP

--- CS-435/Act14/app/src/main/java/cs435/act14/DelayedMessageWorker.kt
+++ CS-435/Act14/app/src/main/java/cs435/act14/DelayedMessageWorker.kt
@@ -2,14 +2,21 @@ package cs435.act14
 
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 class DelayedMessageWorker(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
+        companion object {
+            val messageLiveData = MutableLiveData<String>()
+        }
+
     override fun doWork(): Result {
         val message = inputData.getString("message") ?: "no message"
         Log.d("Delayed Message worker", message)
+
+        messageLiveData.postValue(message)
 
         return Result.success()
     }
--- CS-435/Act14/app/src/main/java/cs435/act14/MainActivity.kt
+++ CS-435/Act14/app/src/main/java/cs435/act14/MainActivity.kt
@@ -1,13 +1,16 @@
 package cs435.act14
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import java.util.concurrent.TimeUnit
 
@@ -23,18 +26,35 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        DelayedMessageWorker.messageLiveData.observe(this) {
+            msg -> Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+
     }
     fun buttonClick(view : View) {
         val message = "42"
 
         val inputData = Data.Builder().putString("message", message).build()
 
+/*
         val workRequest = OneTimeWorkRequestBuilder<DelayedMessageWorker>()
             .setInitialDelay(10, TimeUnit.SECONDS)
             .setInputData(inputData)
             .build()
+*/
+
+        val workRequest = PeriodicWorkRequestBuilder<DelayedMessageWorker>(15, TimeUnit.MINUTES)
+            .setInputData(inputData)
+            .addTag("Delayed message worker")
+            .build()
 
         WorkManager.getInstance(this).enqueue(workRequest)
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Worker Cancellation", "Worker was canceled")
+        WorkManager.getInstance(this).cancelAllWorkByTag("Delayed message worker")
     }
 }
