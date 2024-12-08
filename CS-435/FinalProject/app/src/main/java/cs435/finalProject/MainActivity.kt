253a016f6c7205d2722007f5f04a66523493b8c3
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-12-07 17:44:33 -0700

    CS435 | Final Project ++ Toolbar ++

--- CS-435/FinalProject/app/src/main/java/cs435/finalProject/MainActivity.kt
+++ CS-435/FinalProject/app/src/main/java/cs435/finalProject/MainActivity.kt
@@ -1,8 +1,11 @@
 package cs435.finalProject
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +18,32 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.refresh_action -> {
+                // Refresh logic
+                true
+            }
+
+            R.id.settings_action -> {
+                // Setttings action -> Push into settings fragment
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
