92d001d9fde9f817162c5f96ed32b06e249545ac
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2025-02-03 16:22:14 -0700

    CS 365 | HW3 finished | needs submitting

---
+++ CS-365/HW3.py
@@ -0,0 +1,79 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+database_uri = os.getenv('DATABASE_URL')
+
+client = MongoClient(database_uri, server_api=ServerApi('1'))
+
+try:
+    # P1.a
+    db = client["sample_restaurants"]
+    collections = db.list_collection_names()
+    print(collections)
+    print("======")
+
+    # # P1.b
+    # for collection_name in collections:
+    #     doc_count = db[collection_name].count_documents({})
+    #     print(f"{collection_name}: {doc_count} documents")
+    # print("======")
+    #
+    # # P1.c
+    # for collection_name in collections:
+    #     documents = db[collection_name].find_one()
+    #     print(f"{collection_name} fields: {list(documents.keys())}")
+    # print("======")
+
+    # # P2.a
+    # results = db.restaurants.find({"borough": "Manhattan"}, {"name": 1, "cuisine": 1, "_id": 0})
+    # for doc in results:
+    #     print(doc)
+    # print("======")
+    #
+    # # P2.b
+    # query = {"borough": "Queens", "cuisine": "Italian"}
+    # count = db.restaurants.count_documents(query)
+    # print(f"Italian restaurants in Queens: {count}")
+    # print("======")
+
+    # P3
+    root_restaurant = {
+        "name": "Root Awakening",
+        "cuisine": "Vegetarian",
+        "borough": "Bronx",
+        "address": {
+            "street": "asylum road",
+            "zipcode": "10454"
+        }
+    }
+    db.restaurants.insert_one(root_restaurant)
+    print("Root Awakening - Added to DB")
+    print("======")
+
+    # P4
+    update_grade = {
+        "$set": {
+            "grades": [
+                {"grade": "A", "score": 6},
+                {"grade": "B", "score": 18}
+            ]}}
+    db.restaurants.update_one({"name": "Root Awakening"}, update_grade)
+    print("Updated restaurant")
+    print("======")
+
+    # # P5.a
+    # db.restaurants.delete_many({"borough": "Staten Island", "grades.grade": "C"})
+    # print("Deleted restaurant")
+    # print("======")
+    #
+    # # P5.b
+    # db.restaurants.delete_one({"name": "Root Awakening"})
+    # print("Deleted restaurant - Root Awakening")
+    # print("======")
+
+except Exception as e:
+    print(e)
