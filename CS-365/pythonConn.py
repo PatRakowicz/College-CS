5ca0f9f95ef9a811f1ddcc8491732843a880c5e7
BMac-Dev <patrick.rakowicz@gmail.com>
2025-02-04 08:34:12 -0700

    CS365 | Updated Conn Test

--- CS-365/pythonConn.py
+++ CS-365/pythonConn.py
@@ -11,19 +11,7 @@ database_uri = os.getenv('DATABASE_URL')
 client = MongoClient(database_uri, server_api=ServerApi('1'))
 
 try:
-	# connect to database
-	db = client["sample_mflix"]
-	
-	# iterate over collections in database and print out their names
-#	for coll in db.list_collection_names():
-#       		print(coll)
-	# access specific collection in database
-	
-	movies = db["movies"]
-#	movies.insert_many([{"title": "Marriage Story"},{"title":"Toy Story 4"}])
-
-	print("find all")	
-	print(movies.count_documents({"year":{"$gt": 2015}}))
-
+	client.admin.command('ping')
+	print("Pinged deployment, Connected")	
 except Exception as e:
 	print(e)
